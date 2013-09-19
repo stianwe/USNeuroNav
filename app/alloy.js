4fec1fcbe62554e276820fb5acdb6166ef206b29
Kristoffer <kiffihagen@gmail.com>
2013-09-19 13:27:17 +0200

    Show All test

--- app/alloy.js
+++ app/alloy.js
@@ -23,20 +23,25 @@ var cases = [
 		new classes.mediaFile("http://upload.wikimedia.org/wikipedia/en/9/9d/Maggie_Simpson.png", false, false),
 	]),
 	new classes.caseT("Case 2", new Array()),
-	new classes.caseT("Case 3", new Array()),
-	new classes.caseT("Case 4", new Array()),
+	//new classes.caseT("Case 3", new Array()),
+	//new classes.caseT("Case 4", new Array()),
 ];
 var oneCat = new classes.category("Frontal Lobe", new Array());
 oneCat.cases = cases;
-subCatsCat1[0] = new classes.category("Primary Tumors", [
+
+// i made this for test showall.
+subCatsCat1[0] = new classes.category("Show All", [oneCat]);
+// end
+
+subCatsCat1[1] = new classes.category("Primary Tumors", [
 	oneCat,
 	new classes.category("Occipital Lobe", new Array()),
 	new classes.category("Parietal Lobe", new Array()),
 	new classes.category("Random Lobe", new Array()),
 	new classes.category("Brain Stem", new Array()),
 ]);
-subCatsCat1[1] = new classes.category("Gliomas", new Array());
-subCatsCat1[2] = new classes.category("Meningiomas", new Array());
+subCatsCat1[2] = new classes.category("Gliomas", new Array());
+subCatsCat1[3] = new classes.category("Meningiomas", new Array());
 subCats[0] = new classes.category("Tumors", subCatsCat1);
 subCats[1] = new classes.category("Cranial Vascular", new Array());
 subCats[2] = new classes.category("Spine", new Array());
