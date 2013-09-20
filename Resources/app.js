161ac35c366520277271f85123e82fd1cdef6e6e
Stian Weie <stianwe@gmail.com>
2013-09-20 12:12:04 +0200

    Fix category names

--- Resources/app.js
+++ Resources/app.js
@@ -6,25 +6,27 @@ var subCats = new Array();
 
 var subCatsCat1 = new Array();
 
-var cases = [ new classes.caseT("Case 1", [ new classes.mediaFile("http://techslides.com/demos/sample-videos/small.mp4", true, false), new classes.mediaFile("http://upload.wikimedia.org/wikipedia/en/e/ec/Lisa_Simpson.png", false, false), new classes.mediaFile("http://images.wikia.com/simpsons/images/5/53/Bart_Simpson_Head.PNG", false, false), new classes.mediaFile("http://upload.wikimedia.org/wikipedia/en/9/9d/Maggie_Simpson.png", false, false) ]), new classes.caseT("Case 2", new Array()), new classes.caseT("Case 3", new Array()), new classes.caseT("Case 4", new Array()) ];
+var cases = [ new classes.caseT("Case 1", [ new classes.mediaFile("http://techslides.com/demos/sample-videos/small.mp4", true, false), new classes.mediaFile("http://upload.wikimedia.org/wikipedia/en/e/ec/Lisa_Simpson.png", false, false), new classes.mediaFile("http://images.wikia.com/simpsons/images/5/53/Bart_Simpson_Head.PNG", false, false), new classes.mediaFile("http://upload.wikimedia.org/wikipedia/en/9/9d/Maggie_Simpson.png", false, false) ]), new classes.caseT("Case 2", new Array()) ];
 
-var oneCat = new classes.category("One", new Array());
+var oneCat = new classes.category("Frontal Lobe", new Array());
 
 oneCat.cases = cases;
 
-subCatsCat1[0] = new classes.category("Cat1Subcat1", [ oneCat, new classes.category("Two", new Array()), new classes.category("Three", new Array()), new classes.category("Four", new Array()), new classes.category("Five", new Array()) ]);
+subCatsCat1[0] = new classes.category("Show All", [ oneCat ]);
 
-subCatsCat1[1] = new classes.category("Cat1Subcat2", new Array());
+subCatsCat1[1] = new classes.category("Primary Tumors", [ oneCat, new classes.category("Occipital Lobe", new Array()), new classes.category("Parietal Lobe", new Array()), new classes.category("Random Lobe", new Array()), new classes.category("Brain Stem", new Array()) ]);
 
-subCatsCat1[2] = new classes.category("Cat1Subcat3", new Array());
+subCatsCat1[2] = new classes.category("Gliomas", new Array());
 
-subCats[0] = new classes.category("cat1", subCatsCat1);
+subCatsCat1[3] = new classes.category("Meningiomas", new Array());
 
-subCats[1] = new classes.category("cat2", new Array());
+subCats[0] = new classes.category("Tumors", subCatsCat1);
 
-subCats[2] = new classes.category("cat3", new Array());
+subCats[1] = new classes.category("Cranial Vascular", new Array());
 
-var rootCategory = new classes.category("Root", subCats);
+subCats[2] = new classes.category("Spine", new Array());
+
+var rootCategory = new classes.category("Browse", subCats);
 
 Alloy.Globals.rootCategory = rootCategory;
 
